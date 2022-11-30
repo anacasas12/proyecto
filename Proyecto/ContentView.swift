a5aadcf2633a3a5a7f44e3a507e8be50ed3a33f4
Ana Cecilia Casas Carrion <ana.casas@tecsup.edu.pe>
2022-11-29 16:05:35 -0800

    Initial Commit

---
+++ Proyecto/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Proyecto
+//
+//  Created by Ana Casas on 11/29/22.
+//  Copyright © 2022 Ana Casas. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
